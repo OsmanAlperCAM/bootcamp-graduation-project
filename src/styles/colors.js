5f9698c2c5baea5e682cf0521c5a7ecf144b1aa3
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2021-12-29 18:13:14 +0300

    global style file added

---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+export default {
+  light: {
+    primaryColor: '#fafafa',
+    primaryColorLight: '#ffffff',
+    primaryColorDark: '#c7c7c7',
+    primaryTextColor: '#000000',
+  },
+  dark: {
+    primaryColor: '#37474f',
+    primaryColorLight: '#62727b',
+    primaryColorDark: '#102027',
+    primaryTextColor: '#ffffff',
+  },
+};
