58ec61fb75bf785159860527acc6a16964f7edf0
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-01-05 16:49:00 +0300

    SplashScreen Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
 import Provider from './context/Provider';
-import FlashMessage from "react-native-flash-message";
+import FlashMessage from 'react-native-flash-message';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = props => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider>
       <Navigation />
